76f5335825a832b0eee831c3d1004e99e9e56965
MR. HAN WIN AUNG <66130500807@st.sit.kmutt.ac.th>
2025-03-09 21:30:21 +0700

    change read-the-docs text

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Want to learn more about React & Vite?
       </p>
     </>
   )
